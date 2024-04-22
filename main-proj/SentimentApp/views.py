b9d23665455ff3132233eb4bf8620c9a85c5c081
Decode2-coder <65841088+Decode2-coder@users.noreply.github.com>
2024-04-22 16:51:00 +0530

    Update views.py

--- main-proj/SentimentApp/views.py
+++ main-proj/SentimentApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import SentimentModel
 from .forms import SentimentForm
-from code import SentimentAnalyzer
+from englishcode import predictType
+from hindicode import predictHindi
+from marathicode import predictMarathi
 
 # Create your views here.
 def SentimentApp(request):
@@ -10,10 +12,41 @@ def SentimentApp(request):
     if request.method == 'POST':
         if form.is_valid():
             sent = form.cleaned_data.get('Sentence')    # got the sentence
-            textAns = SentimentAnalyzer(sent)
+            textAns = predictType(sent)
             context['text'] = textAns
         else:
             form = SentimentForm()
     
     context['form'] = form
     return render(request, 'app.html', context=context)
+
+def hindi_page(request):
+    form = SentimentForm(request.POST or None)
+    context = {}
+    if request.method == 'POST':
+        if form.is_valid():
+            sent = form.cleaned_data.get('Sentence')    # got the sentence
+            textAns = predictHindi(sent)
+            context['text'] = textAns
+        else:
+            form = SentimentForm()
+    
+    context['form'] = form
+    return render(request, 'hindi.html', context=context)
+
+
+def marathi_page(request):
+    form = SentimentForm(request.POST or None)
+    context = {}
+    if request.method == 'POST':
+        if form.is_valid():
+            sent = form.cleaned_data.get('Sentence')    # got the sentence
+            textAns = predictMarathi(sent)
+            context['text'] = textAns
+        else:
+            form = SentimentForm()
+    
+    context['form'] = form
+    return render(request, 'marathi.html', context=context)
+
+
