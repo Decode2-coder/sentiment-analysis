bbcc622fd8e80afc4f283ccc6874d644f1a209f8
Decode2-coder <65841088+Decode2-coder@users.noreply.github.com>
2024-04-22 16:50:35 +0530

    Update models.py

--- main-proj/SentimentApp/models.py
+++ main-proj/SentimentApp/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class SentimentModel(models.Model):
-    Sentence = models.CharField(max_length=120)
+    Sentence = models.CharField(max_length=512)
     
